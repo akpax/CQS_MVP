7064db1779b1525b91285b48950637cccff1a71d
akpax <austin.paxton00@gmail.com>
2024-06-19 17:47:58 -0700

    add docustrings, airflow skip exception, retries for pull_bars

--- dags/injest_alpaca_bars.py
+++ dags/injest_alpaca_bars.py
@@ -1,31 +1,28 @@
-import os
-import sys
-
+"""
+This module contains the 'inject_alpaca_bars' dag. The first operator checks if
+the NYSE is open and short circuits if it is closed. If the stock exchange is
+open it proceeds with a python operator that pulls bars from the current day 
+and pushes them to bigquery.
+"""
+import logging
+from datetime import datetime, timedelta, date
+from google.cloud import bigquery
+import pendulum
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.email import EmailOperator
-
-from google.cloud import bigquery
-
-import pendulum
-import pandas as pd
-from datetime import datetime, timedelta, date
-
-import logging
-
-
+from airflow.exceptions import AirflowSkipException
 from util.core.alpaca.alpaca_actions import pull_bars
 from util.core.calendar.calendar_util import check_if_exchange_open
 
 EMAILS = ["austin.paxton007@gmail.com", "fallonjoey1@gmail.com"]
-
 ALPACA_HEADER = (
     Variable.get("SECRET_ALPACA_API_KEY_ID"),
     Variable.get("SECRET_ALPACA_API_SECRET_KEY"),
 )
-
+TABLE_ID = "cqs-mvp.stocks.price-history"
 SCHEMA = [
     bigquery.SchemaField("symbol", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("timestamp", "TIMESTAMP", mode="REQUIRED"),
@@ -38,10 +35,13 @@ SCHEMA = [
     bigquery.SchemaField("vwap", "FLOAT", mode="REQUIRED"),
 ]
 
-TABLE_ID = "cqs-mvp.stocks.price-history"
-
-
 def pull_bars_push_to_bq(**ctx):
+    """
+    This function bulls bars for Alpaca-supported exchanges and then adds them
+    to a BigQuery. Note that the start date is non-inclusive. Also, the end date 
+    must be specified 15 minutes before current time for our current market data 
+    subscription. 
+    """
     yesterday = datetime.now() - timedelta(days=1)
     logging.info(yesterday)
     today = datetime.now() - timedelta(minutes=15)
@@ -49,7 +49,7 @@ def pull_bars_push_to_bq(**ctx):
     bars_df = pull_bars(yesterday, today, ALPACA_HEADER)
     if not bars_df:
         logging.error("NULL DATA")
-        # TODO raise airflow skip exception
+        raise AirflowSkipException
     logging.info(bars_df)
     client = bigquery.Client(project="cqs-mvp")
     job_config = bigquery.LoadJobConfig(schema=SCHEMA)
@@ -57,8 +57,11 @@ def pull_bars_push_to_bq(**ctx):
     # waits for job to complete
     job.result()
 
-
 def is_exchange_open_today():
+    """
+    Checks if New York Stock Exchange is open on the current date. Returns 
+    boolean value.
+    """
     return check_if_exchange_open("NYSE", date.today())
 
 
